25b3bd5719b3a928d6d9a52f17dd4a2ccc207906
phaneendra <phani.manthena27@gmail.com>
2023-07-29 16:45:28 +0530

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -103,11 +103,13 @@ resource "aws_launch_template" "main" {
   image_id               = data.aws_ami.centos8.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  user_data = base64encode(templatefile("${path.module}/user_data.sh", { component = var.component, env = var.env }))
   iam_instance_profile {
     arn = aws_iam_instance_profile.test_profile.arn
   }
-  user_data = base64encode(templatefile("${path.module}/user_data.sh", { component = var.component, env = var.env }))
-
+  instance_market_options {
+    market_type = "spot"
+  }
 }
 resource "aws_lb_target_group" "target_group" {
   name     = "${var.component}-${var.env}"
