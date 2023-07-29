03f1bb8e56fec8bfa561adbf42835052a011a1d6
phaneendra <phani.manthena27@gmail.com>
2023-07-29 14:10:18 +0530

    updating ./tf-module-app

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}-security-group"
+  description = "${var.env}-${var.component}-security-group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.component}-security-group" }
+  )
+}
