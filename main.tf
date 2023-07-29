a2ac504aedf56b29e515bb6b4bdd2da6053d9da1
phaneendra <phani.manthena27@gmail.com>
2023-07-29 16:13:34 +0530

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -24,6 +24,35 @@ resource "aws_iam_instance_profile" "test_profile" {
   role = aws_iam_role.role.name
 }
 
+resource "aws_iam_policy" "policy" {
+  name        = "${var.env}-${var.component}-parameter-store-policy"
+  path        = "/"
+  description = "${var.env}-${var.component}-parameter-store-policy"
+
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+  {
+    "Sid": "VisualEditor0",
+    "Effect": "Allow",
+    "Action": [
+    "ssm:GetParameterHistory",
+    "ssm:GetParametersByPath",
+    "ssm:GetParameters",
+    "ssm:GetParameter"
+  ],
+    "Resource": "arn:aws:ssm:us-east-1:347554562486:parameter/${var.env}.${var.component}*"
+  },
+  {
+    "Sid": "VisualEditor1",
+    "Effect": "Allow",
+    "Action": "ssm:DescribeParameters",
+    "Resource": "*"
+  }
+  ]
+
+}
 resource "aws_security_group" "main" {
   name        = "${var.env}-${var.component}-security-group"
   description = "${var.env}-${var.component}-security-group"
@@ -64,6 +93,9 @@ resource "aws_launch_template" "main" {
   image_id      = data.aws_ami.centos8.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.test_profile.arn
+  }
 
 }
 resource "aws_lb_target_group" "target_group" {
