2127fb287af084d101c121ba21bcfddb63e91bfc
phaneendra <phani.manthena27@gmail.com>
2023-07-31 11:54:28 +0530

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -45,8 +45,8 @@ resource "aws_iam_policy" "policy" {
         "Resource" : [
           "arn:aws:ssm:us-east-1:347554562486:parameter/${var.env}.${var.component}*",
           "arn:aws:ssm:us-east-1:347554562486:parameter/nexus*",
-          "arn:aws:ssm:us-east-1:347554562486:parameter/nexus${var.env}.docdb*"
-          "arn:aws:ssm:us-east-1:347554562486:parameter/nexus${var.env}.elasticache*"
+          "arn:aws:ssm:us-east-1:347554562486:parameter/nexus${var.env}.docdb*",
+          "arn:aws:ssm:us-east-1:347554562486:parameter/nexus${var.env}.elasticache*",
           "arn:aws:ssm:us-east-1:347554562486:parameter/nexus${var.env}.rds*"
         ]
 
